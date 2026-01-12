1096fee5ef21896a37ef9c1094cd506537f87b19
faith-notes <124418034+tannnnnnsdf@users.noreply.github.com>
2026-01-12 10:02:47 +0700

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,6 +1,11 @@
 fetch("header.html")
-  .then(res => res.text())
+  .then(r => r.text())
   .then(html => {
     document.getElementById("header").innerHTML = html;
   });
 
+fetch("footer.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("footer").innerHTML = html;
+  });
